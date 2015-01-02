071adaa5c7cdc6f09cbeb73b59ec4221fc33df8c
Kaldoran <kaldoran@live.fr>
2015-01-02 18:55:17 +0100

    Debut de l'ia

--- ia.pl
+++ ia.pl
@@ -1,6 +1,16 @@
 %% Author : Reynaud Nicolas
 
 eval(Board, Valeur, Pawn) :-
+    isEndGame(Board, Winner),
+    ( 
+        iPlay(Pawn),
+        Winner = Pawn,
+        Valeur = 100;
+        Valeur = -100
+    ).
+
+    
+eval(Board, Valeur, Pawn) :-
     invert_player(Pawn, EnemiPawn),
     findall(Pawn, member(Pawn, Board), ResultP), 
     findall(EnemiPawn, member(EnemiPawn, Board), ResultE), 
@@ -27,26 +37,35 @@ eval(Board, Valeur, Pawn) :-
                 Valeur is -(TotalE - TotalP)
             )
     ).
+    
+% On cherche tout les coup, pour ce faire on cherche les cases vides
 
+minmax(Board, Pawn, Depth) :-
+    applyMoves(Board, [], _, MaxVal, Pawn, 0).
+    
 minmax(Board, Pawn, Depth) :-
     findall(Place, nth1(Place, Board, '   '), BlankSpace),
     seekMoves(Board, BlankSpace, Pawn, Depth).
 
 
-
-seekMoves(_, _, _, 0).
+% Cherche et effectue tout les coup possibles    
 seekMoves(Board, [To|BlankSpace], Pawn, Depth) :-
-    findall(Place, existValideEat(Board, Place, Between, To, ' x '), ResultEat),
-    findall(Place, existValide(Board, Place, To, ' x ', Result),
-    applyMoves(Board, ResultEat, To, MaxVal, Pawn, Depth),
-    applyMoves(Board, Result, To, MaxVal, Pawn, Depth),
-    seekMoves(NewBoard, BlankSpace, Pawn, Depth).
+    findall(Place, existValideEat(Board, Place, Between, To, Pawn), ResultEat),
+    findall(Place, existValide(Board, Place, To, Pawn, Result), Result),
+    applyMoves(NewBoard, ResultEat, To, MaxVal, Pawn, Depth),
+    applyMoves(NewBoard, Result, To, MaxVal2, Pawn, Depth),
 
-applyMoves(Board, [], _, MaxVal, _, _).
+applyMoves(Board, [], _, MaxVal, Pawn, 0) :- 
+    eval(Board, Eval, Pawn),
+    ( 
+        iPlay(Pawn),
+        max(Eval, MaxVal);
+        min(Eval, MaxVal)
+    ).
+        
 applyMoves(Board, [From|Result], To, MaxVal, Pawn, Depth) :-
     NewDepth is Depth - 1,
     move(Board, From, To, Pawn, NewBoard),
-    eval(Board, Valeur, Pawn),
     minmax(NewBoard, Pawn, NewDepth),
     ( Valeur > MaxValue -> 
         applyMoves(Board, Result, To, Valeur, Pawn, Depth);
