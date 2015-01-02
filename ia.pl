2f5d43667fa10e5233eec2afa5034edaf2324e76
Kaldoran <kaldoran@live.fr>
2015-01-02 20:46:10 +0100

    Possibilité de trouver tout les coup possible

--- ia.pl
+++ ia.pl
@@ -49,22 +49,14 @@ min(A, B, A) :-
 min(_, B, B).
 
 findPlay(Board, Pawn, Depth, From, To) :-
+
     minmax(Board, Pawn, Depth, Eval, From, To), write(Eval).
 
 minmax(Board, Pawn, 0, Eval, _, _) :-
     eval(Board, Eval, Pawn).
    
 minmax(Board, Pawn, Depth, Eval, From, To) :-
-    findall(Place, nth1(Place, Board, '   '), BlankSpace),
-    seekMoves(Board, BlankSpace, Pawn, Depth, From, To).
-
-
-% Cherche et effectue tout les coup possibles    
-seekMoves(Board, [To|BlankSpace], Pawn, Depth, From, To) :-
-    findall(Place, existValideEat(Board, Place, Between, To, Pawn), ResultEat),
-    findall(Place, existValide(Board, Place, To, Pawn), Result),
-    applyMoves(NewBoard, ResultEat, To, MaxVal, Pawn, Depth, From),
-    applyMoves(NewBoard, Result, To, MaxVal, Pawn, Depth, From).
+    true.
         
 applyMoves(Board, [From|Result], To, MaxVal, Pawn, Depth, From) :-
     NewDepth is Depth - 1,
@@ -75,48 +67,30 @@ applyMoves(Board, [From|Result], To, MaxVal, Pawn, Depth, From) :-
         applyMoves(Board, Result, To, MaxValue, Pawn, Depth)
     ).
     
-%% findall(Place, nth1(Place, Board, '   '), BlankSpace).
-%% findall(Place, existValideEat(Board, Place, Between, To, ' x '), ' x ').
-%% Et findall(Place, existValide(Board, Place, To, ' x '), ' x ').
-%% Donne la liste des coups
-%% To depuis BlankSpace
+    
+    
+findAllMove(Board, Pawn, AllMoves) :-
+    findall(Place, nth1(Place, Board, '   '), BlankSpace),
+    seekMoves(Board, BlankSpace, Pawn, AllMoves).
 
+seekMoves(_, [], _, []).
+seekMoves(Board, [To|BlankSpace], Pawn, AllMoves) :-
+    findall(Place, existValideEat(Board, Place, Between, To, Pawn), ResultEat),
+    findall(Place, existValide(Board, Place, To, Pawn), ResultValide),
+    allMoves(ResultValide, To, AllMovesValide),
+    allMoves(ResultEat, To, AllMovesEat),
+    append(AllMovesValide, AllMovesEat, AllMovesGlobale),
+    seekMoves(Board, BlankSpace, Pawn, AllMovesSeek),
+    append(AllMovesGlobale, AllMovesSeek, AllMoves).
+
+allMoves([], _, []).
+allMoves([From|Result], To, [[From, To]|AllMoves]) :-
+    allMoves(Result, To, AllMoves).
+    
 %% Puis : Essai move depuis valideMove
 %% Calcul
 %% Best of Calcule
 
-findAllMove(Board, Pawn, AllMoves) :-
-    findall(Place, nth1(Place, Board, '   '), BlankSpace),
-    seekMoves(Board, BlankSpace, AllMoves).
-   
-seekMoves(Board, [To|BlankSpace], AllMoves) :-
-    findall(Place, existValideEat(Board, Place, Between, To, Pawn); existValide(Board, Place, To, Pawn), Result),
-    findall(Place, existValide(Board, Place, To, Pawn), Result),
-    allMoves(ResultEat, To, AllMoves),
-    allMoves(Result, To, AllMoves).  
 
-allMoves(Result, To, AllMoves).
+    
 
-
-
-findall(Place,(existValideEat([ ' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',
-                                  ' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',
-                                  ' w ',' x ',' w ',' x ',' w ',' x ',' w ',' X ',' w ',' x ',
-                                  ' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',
-                                  ' w ',' o ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',
-                                  '   ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',' w ',
-                                  ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
-                                  ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',
-                                  ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
-                                  ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w '
-                                 ], Place, Betwee, 51, ' x '); existValide([ ' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',
-                                  ' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',
-                                  ' w ',' x ',' w ',' x ',' w ',' x ',' w ',' X ',' w ',' x ',
-                                  ' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',
-                                  ' w ','   ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',
-                                  '   ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',' w ',
-                                  ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
-                                  ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',
-                                  ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
-                                  ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w '
-                                 ], Place, 42, ' x ')), Result).
