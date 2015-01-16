7671a3bcff653f484ba333d9d99b47b18621eaf1
Bascool <bascol.kevin@hotmail.fr>
2015-01-16 01:04:33 +0100

    add test 4-6

---
+++ Test/test4.pl
@@ -0,0 +1,23 @@
+% Ce test montre qu'au départ, sans pion adverse à prendre, l'ordinateur vas chercher à augmenter la valeur totale de ses cases.
+
+% Lancez test4. pour voir le résultat.
+
+initialize_game_test4([ ' w ',' X ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',
+				       ' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',
+			 	       ' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',' w ',' x ',
+				       ' x ',' w ',' x ',' w ','   ',' w ',' x ',' w ',' x ',' w ',
+				       ' w ','   ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',
+				       '   ',' w ','   ',' w ','   ',' w ','   ',' w ','   ',' w ',
+				       ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
+				       ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',
+				       ' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',
+				       ' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w ',' o ',' w '
+				      ] ).
+
+test4 :-
+    initialize_game_test4(Board), 
+    printBoard(Board),
+    findPlay(Board, ' o ', 3, Moves, 2), 
+    printIaMove(Moves),
+    multiMove(Board, Moves, NewBoard),
+    printBoard(NewBoard), !.
